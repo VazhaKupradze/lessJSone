734dd90dd7e467593053d1423594722858050f64
VazhaKupradze <94189837+VazhaKupradze@users.noreply.github.com>
2023-06-16 21:51:53 +0400

    upd

---
+++ script.js
@@ -0,0 +1,29 @@
+let num = 100;
+if (num < 50) {
+    console.log("Less than 50");
+}
+else if (num > 20) {
+    console.log("Greater than 20");
+}
+else {
+    console.log(error);
+}
+
+let userName = "mariam";
+if (userName == "mariam") {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+let secName = "mariam"
+switch (secName) {
+    case "mariam":
+        console.log("true");
+        break;
+
+    default:
+        console.log("false");
+        break;
+}
